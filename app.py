f2b2efd622ac5fbf825859a8cd20ce2f24c66942
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2022-03-31 11:38:32 +0300

    CSV to JSON works

--- app.py
+++ app.py
@@ -2,11 +2,51 @@ print("API starting...")
 
 # app.py
 from flask import Flask, request, jsonify
-import pandas
+import json
+import csv
+import os
 
 app = Flask(__name__)
 
-parts = pandas.read_json(r"data/DemoData.json")
+
+def make_parts():
+    csvFilePath = r'data/DemoData.csv'
+    jsonFilePath = r'data/demoData.json'
+
+    if (os.path.exists('data/demoData.json') == True):
+        os.remove('data/demoData.json')
+
+    # create a dictionary
+    data = {}
+     
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+         
+        # Convert each row into a dictionary
+        # and add it to data
+        for rows in csvReader:
+             
+            # Assuming a column named 'No' to
+            # be the primary key
+            key = rows['Id']
+            data[key] = rows
+ 
+    # Open a json writer, and use the json.dumps()
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+    PartsData = open('data/demoData.json')
+    PartsData = json.load(PartsData)
+    return PartsData
+
+
+
+parts = make_parts()
+
+
+
 
 @app.get("/parts")
 def get_parts():
--- test.py
+++ test.py
@@ -1,5 +1,37 @@
-import pandas
+import json
+import csv
+import os
 
-parts = pandas.read_json
+def make_json():
+    csvFilePath = r'data/DemoData.csv'
+    jsonFilePath = r'data/demoData.json'
 
-print(parts)
+    if (os.path.exists('data/demoData.json') == True):
+        os.remove('data/demoData.json')
+
+    # create a dictionary
+    data = {}
+     
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+         
+        # Convert each row into a dictionary
+        # and add it to data
+        for rows in csvReader:
+             
+            # Assuming a column named 'No' to
+            # be the primary key
+            key = rows['Id']
+            data[key] = rows
+ 
+    # Open a json writer, and use the json.dumps()
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+    PartsData = open('data/demoData.json')
+    PartsData = json.load(PartsData)
+    return PartsData
+
+print(make_json())
